06a7b886ef0a6c2c37d9efe86d91971807c750bf
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-04-01 00:20:26 +0530

    make payment summary interactive

--- amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,11 +1,9 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
+import { renderPaymentSummary } from './paymentSummary.js';
 
 const today = dayjs();
 const deliveryDate  = today.add(7, 'days');
@@ -131,6 +129,8 @@ export function renderOrderSummary() {
         
         container.remove();
         updateCartQuantity();
+
+        renderPaymentSummary();
       });
     });
 
@@ -192,6 +192,7 @@ export function renderOrderSummary() {
     quantityLabel.innerHTML = newQuantity;
     
     updateCartQuantity();
+    renderPaymentSummary();
   }
 
   document.querySelectorAll('.js-save-link')
@@ -220,6 +221,7 @@ export function renderOrderSummary() {
         const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
   
